15ecf5115f4bd40b27c2246016bfab8807e77964
embrace_code <embrace_code@163.com>
2025-12-23 11:51:46 +0800

    修改package.json

--- scripts/check.ts
+++ scripts/check.ts
@@ -1,23 +1,23 @@
-#!/usr/bin/env zx
+// #!/usr/bin/env zx
 
-import type { ProcessOutput } from 'zx'
-import { $ } from 'zx'
+// import type { ProcessOutput } from 'zx'
+// import { $ } from 'zx'
 
-import { printObject } from './utils'
+// import { printObject } from './utils'
 
-await $`pnpm spellcheck`.catch((out: ProcessOutput) => {
-  console.log(out)
+// await $`pnpm spellcheck`.catch((out: ProcessOutput) => {
+//   console.log(out)
 
-  throw new Error(out.stdout)
-})
+//   throw new Error(out.stdout)
+// })
 
+// // await Promise.all([$`pnpm type-check`, $`pnpm lint`]).catch((out: ProcessOutput) => {
+// //   printObject(out)
+// //   throw new Error(out.stdout)
+// // })
+
+// // check type and stage
 // await Promise.all([$`pnpm type-check`, $`pnpm lint`]).catch((out: ProcessOutput) => {
 //   printObject(out)
 //   throw new Error(out.stdout)
 // })
-
-// check type and stage
-await Promise.all([$`pnpm type-check`, $`pnpm lint`]).catch((out: ProcessOutput) => {
-  printObject(out)
-  throw new Error(out.stdout)
-})
